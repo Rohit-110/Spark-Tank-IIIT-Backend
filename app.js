5daffa9f33ea566153a8aa8fc804d58756ed7b5f
Rohit Pandey <iit2022019@iiita.ac.in>
2024-06-11 01:46:31 +0530

    updates done

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ export const app = express();
 
 config({path : "./config/config.env"});
 
-app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
