d16e7a56d639b7d9742d43d86d35ade8206a4e75
Rohit Pandey <iit2022019@iiita.ac.in>
2024-06-24 09:02:01 +0530

    updates done

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://spark-tank-iiit-frontend.vercel.app"],
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true,
 }))
